d6051e0ddf759d1b3be9fa9688a86fdb259152c3
Michael Putnam <mikeputnam713@gmail.com>
2022-07-12 11:17:05 -0700

    remove reducer function

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import LogoImage from '../../assets/burger.png'
-import apiCalls from '../../apiCalls'
 import "./nav.scss";
 
 export default function Nav() {
